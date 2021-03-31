68fd7ea630dd252bc4650624c50699ea7481b8e9
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-03-31 12:10:43 +0530

    Update and rename bit_manpulation.cpp to 1st_bit_manpulation.cpp

--- 1st_bit_manpulation.cpp
+++ 1st_bit_manpulation.cpp
@@ -1,3 +1,4 @@
+//count the set bits for a given number
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
@@ -8,18 +9,20 @@ int main()
     cin>>n;
     int c;
     while(n!=0){
-        if(n&1==1)
+        //Apply an bitwise and to check wether the bit is set or not with 1
+        if(n&1==1)//if the bit is set the make count increment
         {
             c++;
         }
-        n=n>>1;
+        n=n>>1;//move right to check the next bits are set or not
     }
     cout<<c<<endl;
-    //by using stl to count set bits
+    //by using stl to count set bits it is an in built function
     int cc=__builtin_popcount(5);
     cout<<cc;
     //for long numbers
     long long num;
+    cin>>num;
     int cl=__builtin_popcountll(num);
     return 0;
 }
